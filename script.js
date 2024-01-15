65e2243f3344dbb375b10bc87d08bd0b40e1dd29
Henrique <henrynyyls@gmail.com>
2024-01-15 15:46:28 -0300

    New function added!
    - I added a function that "counts" time by frames/seconds/minutes and then hours.
    
    - I'm going to see if it works how I want it to, if not, I will change it.

--- script.js
+++ script.js
@@ -2,13 +2,39 @@ const drankWater = document.getElementById("drankWater");
 const list = document.getElementById("list");
 const exit = document.getElementById("exit");
 let currentDate = new Date();
-const minutes = currentDate.getMinutes();
+
+
+let frames = 0;
+let segundos = 0;
+let minutos = 0;
+let horas = 0;
 
 let vezes = 0;
+function verificarPassouDia(){
+  if(frames >= 60){
+    segundos += 1;
+    frames = 0;
+  }
+
+  if(segundos >= 60){
+    minutos += 1;
+    segundos = 0;
+  }
+
+  if(minutos >= 60){
+    horas += 1;
+    minutos = 0;
+  }
+
+  if(horas > 24){
+    horas = 1;
+    minutos = 0;
+  }
+  console.log("Horas: " + horas + " / Minutos: " + minutos + "/ Segundos: " + segundos)
+}
 
 function bebiAgua(){
-  vezes += 1
-  console.log(minutes)
+  vezes += 1;
 }
 
 drankWater.addEventListener("click", function(){
@@ -22,3 +48,10 @@ list.addEventListener("click", function(){
 exit.addEventListener("click", function(){
   
 })
+
+function loop(){
+  verificarPassouDia(frames);
+  frames += 1;
+  requestAnimationFrame(loop);
+}
+loop();
