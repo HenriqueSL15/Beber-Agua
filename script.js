0919661d108ea2214cf9f004572b095f065ff9e9
Henrique <henrynyyls@gmail.com>
2024-01-16 15:51:31 -0300

    - Feeding array with information
    - Now, every 30 mins , a sound will play and only when it happens, you will be able to click on the first button(I will change the visuals later on) and it will push the current date on an array and tell that you drank water, but if you wait some time, it will push on the array the information that you didn't drank water

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 const drankWater = document.getElementById("drankWater");
 const list = document.getElementById("list");
 const exit = document.getElementById("exit");
-let currentDate = new Date();
 
+const alarme = new Audio("/sons/sound1.wav")
+let currentDate = new Date();
+currentDate.toString();
+
+let lista = [];
+
+let tocouSom = false;
+let podeBeber = false;
+
+let contagem = 0;
 
 let frames = 0;
-let segundos = 0;
-let minutos = 0;
+let segundos = 55;
+let minutos = 59;
 let horas = 0;
 
 let vezes = 0;
@@ -19,7 +28,17 @@ function verificarPassouDia(){
   if(segundos >= 60){
     minutos += 1;
     if(minutos == 30 || minutos >= 59){
+      tocarSom();
+      tocouSom = true;
+      podeBeber = true;
+    }
 
+    if(tocouSom){
+      contagem += 1;
+      if(contagem >= 2){
+        lista.push(currentDate + " - NÃO Bebi Água")
+        tocouSom = false
+      }
     }
     segundos = 0;
   }
@@ -33,23 +52,28 @@ function verificarPassouDia(){
     horas = 1;
     minutos = 0;
   }
-  console.log("Horas: " + horas + " / Minutos: " + minutos + "/ Segundos: " + segundos)
+  // console.log("Horas: " + horas + " / Minutos: " + minutos + "/ Segundos: " + segundos);
 }
 
 function bebiAgua(){
+  lista.push(currentDate + " - Bebi Água")
   vezes += 1;
+  podeBeber = false;
 }
 
 function tocarSom(){
-  
+  alarme.play();
+  tocouSom = false;
 }
 
 drankWater.addEventListener("click", function(){
-  bebiAgua();
+  if(podeBeber){
+    bebiAgua();
+  }
 })
 
 list.addEventListener("click", function(){
-  
+  console.log(lista)
 })
 
 exit.addEventListener("click", function(){
